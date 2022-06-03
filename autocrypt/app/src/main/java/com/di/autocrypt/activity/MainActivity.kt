68efc1def7aa8c7eb3b36a363caaf1d4ca6a2bf0
jin7011 <fpdlqlrrja36@naver.com>
2022-06-03 16:38:40 +0900

    location

--- autocrypt/app/src/main/java/com/di/autocrypt/activity/MainActivity.kt
+++ autocrypt/app/src/main/java/com/di/autocrypt/activity/MainActivity.kt
@@ -1,17 +1,27 @@
 package com.di.autocrypt.activity
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.graphics.PointF
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.PermissionChecker
+import androidx.databinding.DataBindingUtil
 import com.di.autocrypt.R
+import com.di.autocrypt.databinding.ActivityMainBinding
 import com.di.autocrypt.model.Repository
 import com.di.autocrypt.model.RoomRepository
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.gun0912.tedpermission.rx3.TedPermission
 import com.naver.maps.geometry.LatLng
-import com.naver.maps.map.MapFragment
-import com.naver.maps.map.NaverMap
-import com.naver.maps.map.NaverMapSdk
-import com.naver.maps.map.OnMapReadyCallback
+import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.overlay.OverlayImage
 import com.naver.maps.map.util.FusedLocationSource
@@ -23,17 +33,19 @@ import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() , OnMapReadyCallback{
+class MainActivity : AppCompatActivity() , OnMapReadyCallback, LocationListener{
 
     @Inject
     lateinit var roomRepository: RoomRepository
-    val LOCATION_REQUEST_CODE = 100
     lateinit var locationSource: FusedLocationSource
+    lateinit var fusedLocationClient: FusedLocationProviderClient
     lateinit var naverMap: NaverMap
+    lateinit var binding:ActivityMainBinding
+    private var locationManager: LocationManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
         NaverMapSdk.getInstance(this).client = NaverMapSdk.NaverCloudPlatformClient("d0g2va8emc")
 
@@ -44,34 +56,82 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback{
             }
 
         mapFragment.getMapAsync(this)
+        locationManager = getSystemService(LOCATION_SERVICE) as LocationManager?
     }
 
     override fun onMapReady(map: NaverMap) {
         this.naverMap = map
-        locationSource = FusedLocationSource(this,LOCATION_REQUEST_CODE)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        requestPermissions()
         map.locationSource = locationSource
-        val uiSettings = map.uiSettings
-        uiSettings.isLocationButtonEnabled = true
-        showMarker()
-    }
+//        val uiSettings = map.uiSettings 기존 내 위치찾기 설정해제
+//        uiSettings.isLocationButtonEnabled = true
+        showMarker() //지도에 마크표시
 
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        //내 위치로 카메라 이동
+        binding.fabTracking.setOnClickListener {
+            if (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    this,Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                map.let {
+                    var currentLocation: Location?
 
-        when(requestCode){
-            LOCATION_REQUEST_CODE -> {
-                locationSource.onRequestPermissionsResult(requestCode,permissions,grantResults)
+                    fusedLocationClient.lastLocation
+                        .addOnSuccessListener { location: Location? ->
+                            currentLocation = location
+                            naverMap.locationOverlay.run {
+                                isVisible = true
+                                position =
+                                    LatLng(currentLocation!!.latitude, currentLocation!!.longitude)
+                            }
+                            // 카메라 현재위치로 이동
+                            val cameraUpdate = CameraUpdate.scrollTo(
+                                LatLng(
+                                    currentLocation!!.latitude,
+                                    currentLocation!!.longitude
+                                )
+                            )
+                            naverMap.moveCamera(cameraUpdate)
+                        }
+                }
             }
         }
     }
 
-    fun showMarker(){
+    override fun onLocationChanged(location: Location) {
+        naverMap.let {
+            val coord = LatLng(location)
 
-        runBlocking {
+            val locationOverlay = it.locationOverlay
+            locationOverlay.isVisible = true
+            locationOverlay.position = coord
+            locationOverlay.bearing = location.bearing
+
+            it.moveCamera(CameraUpdate.scrollTo(coord))
+        }
+    }
+
+    // 위치권한 관련 요청 tedpermission 라이브러리
+    private fun requestPermissions() {
+        // 내장 위치 추적 기능 사용
+        locationSource =
+            FusedLocationSource(this, LOCATION_REQUEST_CODE)
+        TedPermission.create()
+            .setRationaleTitle("위치권한 요청")
+            .setRationaleMessage("현재 위치로 이동하기 위해 위치권한이 필요합니다.") // "we need permission for read contact and find your location"
+            .setPermissions(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            .request()
+            .subscribe({ tedPermissionResult ->
+                if (!tedPermissionResult.isGranted) {
+                    Toast.makeText(this,"거절되었습니다.",Toast.LENGTH_SHORT).show()
+                }
+            }) { throwable -> Log.e("AAAAAA", throwable.message.toString()) }
+    }
+
+    fun showMarker(){
+        runBlocking(Dispatchers.IO){
             val centers = roomRepository.getCenters()
             CoroutineScope(Dispatchers.Main).launch {
                 for(center in centers){
@@ -79,12 +139,12 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback{
                     marker.run {
                         position = LatLng(center.lat.toDouble(), center.lng.toDouble())
                         when(center.centerType){
-                            "중앙/권역" -> marker.icon = OverlayImage.fromResource(R.drawable.location1)
-                            "지역" -> marker.icon = OverlayImage.fromResource(R.drawable.location2)
+                            "중앙/권역" -> marker.icon = OverlayImage.fromResource(com.naver.maps.map.R.drawable.navermap_default_marker_icon_lightblue)
+                            "지역" -> marker.icon = OverlayImage.fromResource(com.naver.maps.map.R.drawable.navermap_default_marker_icon_red)
                             else -> marker.icon = OverlayImage.fromResource(androidx.appcompat.R.drawable.abc_ic_ab_back_material)
                         }
-                        width = resources.getInteger(R.integer.marker_size)
-                        height = resources.getInteger(R.integer.marker_size)
+//                        width = resources.getInteger(R.integer.marker_size)
+//                        height = resources.getInteger(R.integer.marker_size)
                         isIconPerspectiveEnabled = true
                         anchor = PointF(0.5f,1f)
                         map = naverMap
@@ -92,5 +152,34 @@ class MainActivity : AppCompatActivity() , OnMapReadyCallback{
                 }
             }
         }
+    }
+
+//    override fun onRequestPermissionsResult(
+//        requestCode: Int,
+//        permissions: Array<out String>,
+//        grantResults: IntArray
+//    ) {
+//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+//
+//        when(requestCode){
+//            LOCATION_REQUEST_CODE -> {
+//                locationSource.onRequestPermissionsResult(requestCode,permissions,grantResults)
+//                if (ActivityCompat.checkSelfPermission(
+//                        this,
+//                        Manifest.permission.ACCESS_FINE_LOCATION
+//                    ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+//                        this,
+//                        Manifest.permission.ACCESS_COARSE_LOCATION
+//                    ) == PackageManager.PERMISSION_GRANTED
+//                ) {
+//                    locationManager?.requestLocationUpdates(
+//                        LocationManager.GPS_PROVIDER, 1000, 10f, this)
+//                }
+//            }
+//        }
+//    }
+
+    companion object {
+        private const val LOCATION_REQUEST_CODE = 100
     }
 }
